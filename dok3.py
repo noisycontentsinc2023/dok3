819881547db3b4c4933042325974045561d45801
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-18 17:28:40 +0900

    Create dok3.py

---
+++ dok3.py
@@ -0,0 +1,218 @@
+import discord
+import asyncio
+import gspread
+
+from google.oauth2 import service_account
+
+intents = discord.Intents.default()
+intents.members = True
+
+TOKEN = os.environ['TOKEN']
+PREFIX = os.environ['PREFIX']
+
+intents=discord.Intents.all()
+prefix = '!'
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds_info = {
+  "type": "service_account",
+  "project_id": "thematic-bounty-382700",
+  "private_key_id": "502d8dd4f035d15b57bff64ee323d544d28aedc4",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQD4Kze3Hn/yxevG\nzHUklYGSDDs8qKQeyYdd1eWaR0PNKZ2+nwKFGmXGENS6vuy3U81dqI3AVgA3w6UW\nHEaVfPvc31OX5yNCIS0eQxxqWWGJJ5+MbvUC06qXi/7hCup0WK+hoqwjHtUX7AYu\nEDgtf6xd29gSvb3YXs6pvi+2tpwPt0SED6HGPU7qPRsAaPnyUsVCj/mW04ca2iea\nxMsIqxKT6ufNssiXX7qhKyziowneM0lp8BB3k2z+/FGPQOCdi/lIscC9zKbDOIcb\nOZw+B2sd2opp7Dwo3JMIkh3NJevw9hjp0+CFeqemGNsCAiSuFkvydx6BagWaWAPs\nC48nZLNZAgMBAAECggEAF3uUbTMZQZZVoAU5CPYOMY0PfmcJR6IDeX8715BKg8N+\nOhtHBGQJ8Rbm4Ehgcxz+i/AfAK4KnXw5dvkEO1E9Lmph+Tfdg9yKjchlLBGK24z4\nqZPWwpaXl/k+7BnJs2pwbROs5PJeEOJMN+fgPvrrqyJ6RNS4Pf0Dond9AZWwOQLL\naJPFZryK7Bmvtt0H8mDDWdfqmCQTtPJ9PUyDEUeenlyhuek8wH3GHcghOSlsCDF1\nW/3YXM9Vr/arE4V6hTLwXofrUnTuXTfo+DcaOIXpHqIPS+nCyzWZ0kAJ7/uKjhnN\nF4kgv9aXDX9Y7S+3irXazRhowfu2rGuPRO/2+FCuMQKBgQD+JRDctOrKvpl9zDaw\nWT2a3qmYuFf90+b8EkKsWzVBM7neEJlw1ZWxUZzkdHXwkxcM7w93BjZeXJRnI7HZ\n5sHMrRw3p7Cwy0REqC3GSbYMCIZ/98y5Ot5sOXamUCOtYnic1NG2PBzr9h94Nt7d\nZu9D7cD/kaogm9Fl9t1VMD3REQKBgQD5+vvxY0nUkzrPEHfAOnPRqt3fM9ryzmk9\n8WyffmWqaDcvb9pl1F/+/51u00cvh2Q6etvL0J850AB0AKC9QdYzIaSj4LeRNzjA\ns+K6Po5+HAYezxC1cYzFh+slAfX3gX9pa11f3aOltj4h7IXvqBB0iH4rl/i2KQ/G\ntSDa62K9yQKBgAXXEDYiKisSijBr2u3efx3p8/fAdLUug2ZTfRi819Jxv9msg/ol\nzlTOzU4qpvMqTiNL8w0HJYSxl+9u0I1zUgzEBZv5zIOjiCQTwUmHNBm+sGiMZzXy\ndl4CTAmyWb+IPcFM2qzXYMrDUyHOEP0BeooTEpZM4J3zNrKjI57rhuAhAoGAKWDC\nE1K8BdPZCC1RpSAHy8zcrPWIaGiCQx6TPFNPwMU/XTrGi9R7j1oAVTfjsJpYnNV5\nTGNb99XWPV1dPfaH3i7TcczglcjuO/eKsAlqzLUWzkK4IVCKXKgC5D1O2Yk17d03\nt4aYb/Wak0LzaJgJIUD2oYCmSoDBe8K/jX0o+wECgYBnxk9HR/23hjWaxrSnXGDB\nHxLXg9Wz5w0N+gdC/FNxknFOft+nsCMKWMocOtGYhJU3OvkTYYqL1iDsKoMb74xG\nVwB1fuoNrNp+aJ/CzbtZVT1WLzXG41e9cu2TuOy+wpDlryfJAZ6KNVgDOmhh8TR2\nz7T0rt1QSfOZILpiwpR4jg==\n-----END PRIVATE KEY-----\n",
+  "client_email": "noisycontents@thematic-bounty-382700.iam.gserviceaccount.com",
+  "client_id": "107322055541690533468",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/noisycontents%40thematic-bounty-382700.iam.gserviceaccount.com"
+}
+creds = service_account.Credentials.from_service_account_info(info=creds_info, scopes=scope)
+client = gspread.authorize(creds)
+
+#------------------------------------------------투표------------------------------------------------------#  
+def get_emoji(emoji):
+    if isinstance(emoji, str):
+        return emoji
+    elif isinstance(emoji, discord.Emoji):
+        return f'{emoji.name}:{emoji.id}'
+    elif isinstance(emoji, discord.PartialEmoji):
+        return f'{emoji.name}:{emoji.id}'
+    else:
+        return None
+
+polls = {}
+
+@bot.command(name='투표')
+async def vote(ctx, *, args):
+    if not args:
+        embed = discord.Embed(title=f'Vote Help', description=f'')
+        embed.add_field(name=f'Like/Dislike', value=f'!vote title')
+        embed.add_field(name=f'multiple options (1-9)', value=f'!vote title, option 1, option 2, ..., option 9')
+        await ctx.send(embed=embed)
+    else:
+        # Split title and options
+        parts = [part.strip() for part in args.split(',')]
+        title = parts[0]
+        options = parts[1:]
+        # rest of the code
+
+        # Create embed
+        embed = discord.Embed(title=title)
+        if not options:
+            # Like/Dislike
+            message = await ctx.send(embed=embed)
+            await message.add_reaction('👍')
+            await message.add_reaction('👎')
+        else:
+            # Multiple responses (1-9)
+            emoji_list = [chr(0x31) + '\u20E3', chr(0x32) + '\u20E3', chr(0x33) + '\u20E3', chr(0x34) + '\u20E3', chr(0x35) + '\u20E3', chr(0x36) + '\u20E3', chr(0x37) + '\u20E3', chr(0x38) + '\u20E3', chr(0x39) + '\u20E3'] # Option number label
+
+            s = ''
+            emoji = iter(emoji_list)
+            unicode_options = []  # New list for storing Unicode representation of options
+            for option in options:
+                try:
+                    current_emoji = next(emoji)                    
+                    s += f'{current_emoji} {option}\n'
+                    unicode_options.append(current_emoji)
+                except StopIteration:
+                    await ctx.send('Maximum of 9 options allowed.')
+                    return
+
+            # Output title and poll ID to Discord
+            embed.add_field(name='Options', value=s)
+            embed.add_field(name='현재 투표 현황', value='투표를 시작하신 후에 확인이 가능합니다.')
+
+            # Send poll message
+            random_poll_id = str(random.randint(1000, 9999))
+            poll_message = await ctx.send(f'투표가 생성되었어요! 투표 번호는: {random_poll_id}', embed=embed)
+
+            # Add reactions to poll message
+            for i in range(len(options)):
+                await poll_message.add_reaction(emoji_list[i])
+
+            # Save poll information
+            poll_info = {'title': title, 'options': unicode_options, 'votes': {}, 'closed': False, 'message_id': poll_message.id} # Use unicode_options instead of options
+            polls[poll_message.id] = poll_info
+            
+#------------------------------------------------고정------------------------------------------------------# 
+
+sheet1 = client.open('서버기록').worksheet('독독독')
+rows = sheet4.get_all_values()
+
+sticky_messages = {}
+
+for row in rows:
+    sticky_messages[int(row[0])] = row[1]
+    
+def has_specific_roles(allowed_role_ids):
+    async def predicate(ctx):
+        allowed_roles = [ctx.guild.get_role(role_id) for role_id in allowed_role_ids]
+        return any(role in ctx.author.roles for role in allowed_roles)
+
+    return check(predicate)
+
+allowed_role_ids = [922400231549722664, 1019164281696174180]    
+    
+# 스프레드시트에서 초기 고정 메시지를 가져옵니다.
+sticky_messages = {}
+last_sticky_messages = {}
+
+sheet4_values = sheet4.get_all_values()
+for row in sheet4_values:
+    if len(row) == 2 and row[0].isdigit():
+        channel_id = int(row[0])
+        message = row[1]
+        sticky_messages[channel_id] = message
+
+def refresh_sticky_messages():
+    global sticky_messages
+    global last_sticky_messages
+    sheet1_values = sheet4.get_all_values()
+
+    new_sticky_messages = {}  # 반복문 바깥에서 선언합니다.
+    for row in sheet1_values:
+        if len(row) == 2 and row[0].isdigit():
+            channel_id = int(row[0])
+            message = row[1]
+            new_sticky_messages[channel_id] = message  # 값을 할당합니다.
+
+    deleted_channel_ids = set(sticky_messages.keys()) - set(new_sticky_messages.keys())
+    for channel_id in deleted_channel_ids:
+        if channel_id in last_sticky_messages:
+            old_message = last_sticky_messages[channel_id]
+            try:
+                asyncio.create_task(old_message.delete())  # asyncio.create_task를 사용하여 비동기로 실행합니다.
+            except discord.NotFound:
+                pass
+
+    sticky_messages = new_sticky_messages
+    last_sticky_messages = {}
+
+@bot.command(name='고정')
+@has_specific_roles(allowed_role_ids)
+async def sticky(ctx, *, message):
+    global sticky_messages
+    channel_id = ctx.channel.id
+    sticky_messages[channel_id] = message
+
+    # 스프레드시트에 고정 메시지를 저장합니다.
+    if str(channel_id) in sheet4.col_values(1):
+        row_num = int(sheet4.col_values(1).index(str(channel_id))) + 1
+    else:
+        row_num = len(sheet4.col_values(1)) + 1
+
+    sheet1.update_cell(row_num, 1, str(channel_id))
+    sheet1.update_cell(row_num, 2, message)
+
+    # 스프레드시트에 저장된 내용을 업데이트합니다.
+    refresh_sticky_messages()
+
+    await ctx.send(f'메시지가 고정됐습니다!')
+
+@bot.command(name='해제')
+@has_specific_roles(allowed_role_ids)
+async def unsticky(ctx):
+    global sticky_messages
+    channel_id = ctx.channel.id
+
+    if channel_id in sticky_messages:
+        del sticky_messages[channel_id]
+
+        # 스프레드시트에서 고정 메시지를 삭제합니다.
+        row_num = int(sheet4.col_values(1).index(str(channel_id))) + 1
+        sheet4.delete_row(row_num)
+
+        # 스프레드시트에 저장된 내용을 업데이트합니다.
+        refresh_sticky_messages()
+
+        await ctx.send('고정이 해제됐어요!')
+    else:
+        await ctx.send('이 채널에는 고정된 메시지가 없어요')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    await bot.process_commands(message)
+
+    global sticky_messages
+    global last_sticky_messages
+
+    channel_id = message.channel.id
+
+    if channel_id in sticky_messages:
+        if channel_id in last_sticky_messages:
+            old_message = last_sticky_messages[channel_id]
+            try:
+                await old_message.delete()
+            except discord.NotFound:
+                pass
+
+        new_message = await message.channel.send(sticky_messages[message.channel.id])
+        last_sticky_messages[message.channel.id] = new_message
+        
+#봇 실행
+bot.run(TOKEN)
