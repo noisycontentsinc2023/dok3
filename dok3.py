edc851938411876433c50a56311cad1a4adc69da
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-04-25 11:11:36 +0900

    Update dok3.py

--- dok3.py
+++ dok3.py
@@ -12,6 +12,7 @@ from discord import Interaction
 from discord.ext import tasks
 from discord.ext import commands
 from discord.ext.commands import Context
+from discord.ui import Select, SelectOption
 
 intents = discord.Intents.default()
 intents.message_content = True
